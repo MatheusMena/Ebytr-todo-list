57d16124b2f8ce37daab4c5f0db7c163718c8a35
MatheusMena <matheusmena1@hotmail.com>
2022-07-01 17:46:00 -0300

    feat: creating Header protoType

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+
+export default function Header() {
+  return (
+    <header className='w-full  py-8 flex items-center justify-center bg-gray-700 border-b border-gray-600'>
+        <h1 className="text-[40px] font-thin underline decoration-solid">Digital Planner</h1>
+    </header>
+      
+  )
+}
