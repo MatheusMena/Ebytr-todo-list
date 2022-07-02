dc2b6eaff9331a19075b036194d6db60f119a8ba
MatheusMena <matheusmena1@hotmail.com>
2022-07-01 21:19:14 -0300

    feat: Importing  Style

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export default function Header() {
   return (
-    <header className='w-full  py-8 flex items-center justify-center bg-gray-700 border-b border-gray-600'>
+    <header className='w-full  py-4 text-zinc-100 flex items-center justify-center bg-green-700 border-b border-gray-600'>
         <h1 className="text-[40px] font-thin underline decoration-solid">Digital Planner</h1>
     </header>
       
